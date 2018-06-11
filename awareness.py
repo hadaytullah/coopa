73bebcf4bf7c9a7e8622dd2d80bf710b2e64f574
Hadaytullah <hadaytul@lm7-cs-48.local>
2018-06-11 18:10:12 +0300

    removed unused code

--- awareness.py
+++ awareness.py
@@ -190,38 +190,3 @@ class Awareness:
                 elif message.position_of is 'recharge_point':
                     self._knowledge_base.recharge_point_positions.append([message.x, message.y])
 
-    def _move_zig_zag_strategy(self, point):
-        #Move in zig zag fashion, why not :)
-        #print('-- destination point {}'.format(point))
-        #pdb.set_trace()
-        #moore: up,down,left,right and diagonal movements
-        #von neumann: up, down, left, right
-        # include_center = false, mean do not consider its current location
-        #possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
-        possible_steps = []
-        for cell in self._agent.model.grid.iter_neighborhood(self._agent.pos, moore=True):
-            #print('cell is empty {}'.format(cell))
-            if self._agent.model.grid.is_cell_empty(cell):
-                #print('cell is empty {}'.format(cell))
-                possible_steps.append(cell)
-
-        if len(possible_steps) > 0:
-            # find the step that takes the agent closer to a resource
-            # assuming that other resources exisits in proximity of a found resource
-            x_distance_shortest = abs(possible_steps[0][0] - point[0])
-            y_distance_shortest = abs(possible_steps[0][1] - point[1])
-            new_position = possible_steps[0]
-
-            for step in possible_steps:
-                x_distance = abs(step[0] - point[0])
-                y_distance = abs(step[1] - point[1])
-                #print('step:{}, x_distance:{} , y_distance:{}'.format(step, x_distance, y_distance))
-                if x_distance <= x_distance_shortest and y_distance <= y_distance_shortest:
-                    new_position = step
-                    #print('new position for agent#{}: {}'.format(self.unique_id, new_position))
-                    x_distance_shortest = x_distance
-                    y_distance_shortest = y_distance
-
-            #new_position = random.choice(possible_steps)
-            #print('new position for agent#{}: {}'.format(self.unique_id, new_position))
-            self.model.grid.move_agent(self, new_position)
